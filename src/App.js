b536ccb786fc1033db1be3023204798af3456112
Umaid Hassan <23503271+umaid-hassan@users.noreply.github.com>
2021-05-29 12:42:41 +0500

    changes for subpages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import routes from './components/pages'
 function App() {
 
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           {
             routes.map((route, idx) => (
               <Route exact path={route.path} component={route.component} key={idx}></Route>
